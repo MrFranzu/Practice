05fdcbf2634fe181e34bb41ce256dcc0f450d605
Amiel Franz Bagui <118027434+MrFranzu@users.noreply.github.com>
2022-11-21 18:42:27 +0800

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,92 @@
+#Nanatsu no Taizai
+
+class Ban():
+    def sin(self):
+        print("Greed")
+  
+    def animal(self):
+        print("Fox")
+  
+    def weapon(self):
+        print("Holy Staff Courechouse")
+  
+class Diane():
+    def sin(self):
+        print("Envy")
+  
+    def animal(self):
+        print("Serpent")
+  
+    def weapon(self):
+        print("War Hammer Gideon")
+
+class Escanor():
+    def sin(self):
+        print("Pride")
+  
+    def animal(self):
+        print("Lion")
+  
+    def weapon(self):
+        print("Divine Axe Rhitta")
+
+class Gowther():
+    def sin(self):
+        print("Lust")
+  
+    def animal(self):
+        print("Goat")
+  
+    def weapon(self):
+        print("Immortal")
+
+class Merlin():
+    def sin(self):
+        print("Gluttony")
+  
+    def animal(self):
+        print("Boar")
+  
+    def weapon(self):
+        print("Morning Star Aldan")
+
+class King():
+    def sin(self):
+        print("Sloth")
+  
+    def animal(self):
+        print("Grizzly")
+  
+    def weapon(self):
+        print("Spirit Spear Chastiefol")
+
+class Meliodas():
+    def sin(self):
+        print("Wrath")
+  
+    def animal(self):
+        print("Dragon")
+  
+    def weapon(self):
+        print("Demon Sword Lostvayne")
+ 
+def function(object):
+    object.sin()
+    object.animal()
+    object.weapon()
+  
+obj_ban = Ban()
+obj_dia = Diane()
+obj_esc = Escanor()
+obj_gow = Gowther()
+obj_mer = Merlin()
+obj_kin = King()
+obj_mel = Meliodas()
+ 
+function(obj_ban)
+function(obj_dia)
+function(obj_esc)
+function(obj_gow)
+function(obj_mer)
+function(obj_kin)
+function(obj_mel)
